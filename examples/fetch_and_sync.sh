99ca01b02dce0bd0455e325a75e39639180b65a0
Gaute Hope <eg@gaute.vetsj.com>
2014-11-01 08:58:05 +0100

    fetch_and_sync example

---
+++ examples/fetch_and_sync.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#
+# do sync according to strategy in README.md
+#
+
+db=/home/gaute/.mail/
+qry="path:gaute.vetsj.com/**"
+
+echo "=> full sync on $db.."
+last_sync_rev=$HOME/.config/astroid/last_sync_rev
+if [ -e ${last_sync_rev} ]; then
+  lastrev=$(cat ${last_sync_rev})
+else
+  lastrev=0
+fi
+revnow=$(./notmuch_get_revision $db)
+
+# sync tags local-to-remote
+./keywsync -m $db -q "$qry AND lastmod:${lastrev}..${revnow}" -t -v
+
+before_offlineimap=$( date +%s )
+
+# sync maildir <-> imap
+offlineimap
+
+# check for new messages, including doing any auto-tagging in
+# a post-new hook.
+notmuch new
+
+# sync tags remote-to-local
+./keywsync -m $db -q "$qry" --mtime ${before_offlineimap} -k -v
+
+# store revision of last sync
+./notmuch_get_revision $db > ${last_sync_rev}
+
