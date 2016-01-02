7cee7b1184b75bbbd369446bb1aa0adf57a90ba8
Gaute Hope <eg@gaute.vetsj.com>
2016-01-02 17:46:49 +0100

    increment count on skipped message too

--- keywsync.cc
+++ keywsync.cc
@@ -287,6 +287,7 @@ int main (int argc, char ** argv) {
     if (paths.size() == 0) {
       cout << "no files with x-keywords header, skipping message." << endl;
       skipped_messages++;
+      count++;
       continue;
     }
 
